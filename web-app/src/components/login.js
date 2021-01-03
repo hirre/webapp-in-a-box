b41b353d82315c0af8a3ee4cb6cab019074b313c
Hirad Asadi <hirad.asadi@gmail.com>
2021-01-03 23:11:40 +0100

    Login page updates.

--- web-app/src/components/login.js
+++ web-app/src/components/login.js
@@ -1,24 +1,58 @@
-import React from 'react';
-import { Button, Input, TextField } from '@material-ui/core';
+import { Button, TextField, Grid, Typography } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& .MuiTextField-root': {
+      margin: theme.spacing(1),
+      width: '25ch',
+    },
+  },
+}));
 
 function Login() 
 {
+    const classes = useStyles();
+
     return (
-        <div>
-            <form>
-                <Input id="regular" inputProps={{placeholder: "Regular" }} formControlProps={{ fullWidth: true }} />
-                <br/>        
-                <TextField
-                    id="outlined-password-input"
-                    label="Password"
-                    type="password"
-                    autoComplete="current-password"
-                    variant="outlined"
-                />
-                <br/>
-                <Button variant="contained" color="primary">Login</Button>
-            </form>
-        </div>      
+        
+            <Grid
+                container
+                spacing={0}
+                direction="column"
+                alignItems="center"
+                justify="center"
+                style={{ minHeight: '100vh' }}
+            >
+
+                <Grid item xs={3}>
+                    <form className={classes.root} noValidate autoComplete="on">
+                        <Typography variant="h4" component="h2" direction="center">
+                        Login
+                        </Typography>
+                        <br/>
+                        <TextField                    
+                        id="username"
+                        label="Username"
+                        defaultValue=""
+                        variant="outlined"
+                        />
+                        <br/>        
+                        <TextField
+                            id="password"
+                            label="Password"
+                            type="password"
+                            autoComplete="current-password"
+                            variant="outlined"
+                        />
+                        <br/>
+                        <div dir="rtl">
+                            <Button variant="contained" color="primary">Login</Button>
+                        </div>                    
+                    </form>
+                </Grid>   
+
+            </Grid>      
     );
   }
   
