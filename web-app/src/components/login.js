83b6b1aa843050f3ea8f24a18c084d29e9ff243c
Hirad Asadi <hirad.asadi@gmail.com>
2021-01-04 00:24:49 +0100

    Updates.

--- web-app/src/components/login.js
+++ web-app/src/components/login.js
@@ -1,6 +1,5 @@
 import { Button, TextField, Grid, Typography } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
-import '../App.css'
 
 const useStyles = makeStyles((theme) => ({
   root: {
