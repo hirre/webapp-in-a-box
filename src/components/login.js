67c6721e66c9fc122415ace836340fe530342e66
Hirad Asadi <hirad.asadi@gmail.com>
2021-01-04 21:44:06 +0100

    Added spacing and changed layout component.

--- src/components/login.js
+++ src/components/login.js
@@ -1,59 +1,48 @@
-import { Button, TextField, Grid, Typography } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
+import { Button, TextField, Typography, Box } from '@material-ui/core';
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    '& .MuiTextField-root': {
-      margin: theme.spacing(1),
-      width: '25ch',
-    },
-  },
-}));
 
 function Login() 
 {
-    const classes = useStyles();
-
     return (
         
-            <Grid
-                container
-                spacing={0}
-                direction="column"
-                alignItems="center"
-                justify="center"
-                style={{ minHeight: '100vh' }}
-            >
-
-                <Grid item xs={3}>
-                    <form className={classes.root} noValidate autoComplete="on">
-                        <Typography variant="h4" component="h2" direction="center">
-                        Login
-                        </Typography>
-                        <br/>
-                        <TextField                    
-                        id="username"
-                        label="Username"
-                        defaultValue=""
-                        variant="outlined"
-                        />
-                        <br/>        
-                        <TextField
-                            id="password"
-                            label="Password"
-                            type="password"
-                            autoComplete="current-password"
-                            variant="outlined"
-                        />
-                        <br/>
-                        <div dir="rtl">
-                            <Button variant="contained" color="primary">Login</Button>
-                        </div>
-                        <div>Sign up</div>                    
-                    </form>
-                </Grid>   
-
-            </Grid>      
+      <Box
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        minHeight="100vh"
+      >
+        <form noValidate autoComplete="on">
+            <Typography variant="h4" component="h2" direction="center">
+            Login
+            </Typography>
+            <br/>
+            <TextField                    
+            id="username"
+            label="Username"
+            defaultValue=""
+            variant="outlined"
+            InputProps={{
+              style: { marginBottom: 10 }
+              }}
+            />
+            <br/>        
+            <TextField
+                id="password"
+                label="Password"
+                type="password"
+                autoComplete="current-password"
+                variant="outlined"
+                InputProps={{
+                  style: { marginBottom: 10 }
+                  }}
+            />
+            <br/>
+            <div dir="rtl">
+                <Button variant="contained" color="primary">Login</Button>
+            </div>
+            <div>Sign up</div>                    
+        </form>
+      </Box>
     );
   }
   
