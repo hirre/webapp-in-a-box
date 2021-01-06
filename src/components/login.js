6cac1db80cf6995aa902ca9eb5018a13ca362a7d
Hirad Asadi <hirad.asadi@gmail.com>
2021-01-06 01:47:27 +0100

    Token test.

--- src/components/login.js
+++ src/components/login.js
@@ -5,70 +5,69 @@ function Login()
   async function handleLogin(e) 
   {
     e.preventDefault();
-    const requestOptions = 
+    const requestOptions =
     {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ username: 'test', password: 'haha' })
+      body: JSON.stringify({ username: 'testuser', password: 'testpwd' })
     };
 
     const response = await fetch("https://localhost/api/Auth/User", requestOptions);
-    
+
     if (response.ok)
     {
-      const data = await response.json();
-      console.log(data);
+      const token = await response.text();
+      console.log(token);
     }
   }
 
   return (
-        
-      <Box
-        display="flex"
-        justifyContent="center"
-        alignItems="center"
-        minHeight="100vh"
-      >
-        <form noValidate autoComplete="on">
-            <Typography variant="h4" component="h2" direction="center">
-            Login
+
+    <Box
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+      minHeight="100vh"
+    >
+      <form noValidate autoComplete="on">
+        <Typography variant="h4" component="h2" direction="center">
+          Login
             </Typography>
 
-            <br/>
+        <br />
 
-            <TextField                    
-            id="username"
-            label="Username"
-            defaultValue=""
-            variant="outlined"
-            InputProps={{
-              style: { marginBottom: 10 }
-              }}
-            />
+        <TextField
+          id="username"
+          label="Username"
+          defaultValue=""
+          variant="outlined"
+          InputProps={{
+            style: { marginBottom: 10 }
+          }}
+        />
 
-            <br/>
+        <br />
 
-            <TextField
-                id="password"
-                label="Password"
-                type="password"
-                autoComplete="current-password"
-                variant="outlined"
-                InputProps={{
-                  style: { marginBottom: 10 }
-                  }}
-            />
-            
-            <br/>
-            
-            <div dir="rtl">
-                <Button variant="contained" color="primary" onClick={handleLogin}>Login</Button>
-            </div>
-            <div>Sign up</div>                    
-        </form>
-      </Box>
-    );
-  }
-  
-  export default Login;
-  
+        <TextField
+          id="password"
+          label="Password"
+          type="password"
+          autoComplete="current-password"
+          variant="outlined"
+          InputProps={{
+            style: { marginBottom: 10 }
+          }}
+        />
+
+        <br />
+
+        <div dir="rtl">
+          <Button variant="contained" color="primary" onClick={handleLogin}>Login</Button>
+        </div>
+        <div>Sign up</div>
+      </form>
+    </Box>
+  );
+}
+
+export default Login;
